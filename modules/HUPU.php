6f5bf69ec81a19b494ab2f9e648940370685fae7
MantleBox <zhaojingyu2001@outlook.com>
2018-01-12 20:20:35 +0800

    Added HUPU
    gender, favorite sports, nickname and the location of a user

---
+++ modules/HUPU.php
@@ -0,0 +1,16 @@
+<?php
+$op1=array(
+    generate_outputArray("hupu_","gender","gender",'//*[@id="content"]/table[1]/tbody/tr[1]/td[2]',"xpath"),
+    generate_outputArray("hupu_","likedSports","sports",'//*[@id="content"]/table[2]/tbody/tr[1]/td[2]',"xpath"),
+);
+
+add_pattern("hupu","hupuID","https://my.hupu.com/%%/profile",$op1)
+echo "hupuA";
+
+$op1=array(
+    generate_outputArray("hupu_","nickname","nickname",'//*[@id="main"]/div[1]/div[2]/h3/div/text()',"xpath"),
+    generate_outputArray("hupu_","location","location",'//*[@id="main"]/div[1]/div[2]/div/span[4]','xpath'),
+);
+
+add_pattern("hupu","hupuID","https://my.hupu.com/%%",$op1)
+echo "hupuB";
